6f0253b3cc9b3bf781e4f2f284793fc265fa98e3
Jiemin Zhang <jiemin.zhang@santander.de>
2023-03-07 16:04:14 +0100

    conversion service

---
+++ currency-exchange-service/src/test/java/de/sant/microservices/currencyexchangeservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sant.microservices.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
