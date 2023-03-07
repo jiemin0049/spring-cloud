a838ac57aaff2105129b2995d799fd16544aab0a
Jiemin Zhang <jiemin.zhang@santander.de>
2023-03-07 17:13:33 +0100

    conversion service

--- currency-conversion-service/src/main/java/de/sant/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/de/sant/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,19 +1,27 @@
 package de.sant.microservices.currencyconversionservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
+
+    @Autowired
+    private CurrencyExchangeServiceProxy proxy;
 
     @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion convertCurrency(
             @PathVariable String from,
             @PathVariable String to,
             @PathVariable BigDecimal quantity) {
-        return new CurrencyConversion(1L, "USD", "CNY", BigDecimal.ONE, quantity, BigDecimal.valueOf(3000), 8100);
+
+        CurrencyConversion response = proxy.retrieveExchangeValue(from, to);
+        return new CurrencyConversion(response.getId(), from, to, response.getQuantity().multiply(response.getConversionMultiple()), response.getQuantity(), BigDecimal.valueOf(3000), 8100);
     }
 }
