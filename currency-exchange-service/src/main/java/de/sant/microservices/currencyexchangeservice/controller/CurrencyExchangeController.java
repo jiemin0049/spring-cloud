82bdc5118358e1b26e1ae1b51817bd2d8ac5bfc5
jiemin0049 <jiemin0049@gmail.com>
2023-03-07 23:20:27 +0100

    exchange

--- currency-exchange-service/src/main/java/de/sant/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/de/sant/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -6,16 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-
 @RestController
 public class CurrencyExchangeController {
 
     @Autowired
     Environment environment;
 
+    @Autowired
+    ExchangeValueRepository repository;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-        return new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65), Integer.valueOf(environment.getProperty("local.server.port")));
+        ExchangeValue value = repository.findByFromAndTo(from, to);
+        value.setPort(Integer.valueOf(environment.getProperty("local.server.port")));
+        return value;
     }
 }
---
+++ currency-exchange-service/src/main/java/de/sant/microservices/currencyexchangeservice/controller/ExchangeValueRepository.java
@@ -0,0 +1,7 @@
+package de.sant.microservices.currencyexchangeservice.controller;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+    ExchangeValue findByFromAndTo(String from, String to);
+}
