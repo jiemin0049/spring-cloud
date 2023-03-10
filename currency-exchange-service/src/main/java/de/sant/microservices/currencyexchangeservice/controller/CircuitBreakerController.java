bf8b8b72532bec180c98353db08bead70c3a95b2
Jiemin Zhang <jiemin.zhang@santander.de>
2023-03-10 16:49:21 +0100

    currenc exchange

---
+++ currency-exchange-service/src/main/java/de/sant/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -0,0 +1,28 @@
+package de.sant.microservices.currencyexchangeservice.controller;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping("/sample-api")
+    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    public String sampleApi() {
+        logger.info("Sample API called!");
+        ResponseEntity<String> entity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+        return entity.getBody();
+        //return "SampleAPI";
+    }
+
+    public String hardcodedResponse(Exception ex) {
+        return "fallback-response";
+    }
+}
