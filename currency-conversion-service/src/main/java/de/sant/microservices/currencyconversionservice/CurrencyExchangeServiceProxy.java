136a3188da3e7acbb7a1a150ee8e09cae9c0727f
Jiemin Zhang <jiemin.zhang@santander.de>
2023-03-10 16:48:42 +0100

    currenc exchange

--- currency-conversion-service/src/main/java/de/sant/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/de/sant/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -4,9 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
-//@FeignClient(name = "currency-exchange-service") //port in config
-//@RibbonClient(name = "currency-exchange-service") // load balancing
+//@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+@FeignClient(name = "currency-exchange-service") //naming server takes over the port and load balancing through feign, see loadbalancer is under eureka-client dependency
 public interface CurrencyExchangeServiceProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     CurrencyConversion retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
